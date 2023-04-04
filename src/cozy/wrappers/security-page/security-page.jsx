9a766367412e0e4f96d68ea4e9aef070806b439d
Théo Poizat <hello@zatteo.com>
2023-04-04 16:32:02 +0200

    refactor: Remove unused onSkipExtension
    This method was copy pasted but it is only used in InstallationStep.

--- src/cozy/wrappers/security-page/security-page.jsx
+++ src/cozy/wrappers/security-page/security-page.jsx
@@ -7,20 +7,18 @@ import { STEPS } from 'cozy/react/steps'
 
 const SecurityPageWrapper = ({
   reactWrapperProps,
-  onSkipExtension,
   navigate,
 }) => {
   return (
     <ReactWrapper reactWrapperProps={reactWrapperProps}>
       <OnboardingStepper route={STEPS.SECURITY} navigate={navigate}/>
-      <SecurityPage onSkipExtension={onSkipExtension} navigate={navigate}></SecurityPage>
+      <SecurityPage navigate={navigate}></SecurityPage>
     </ReactWrapper>
   );
 };
 
 SecurityPageWrapper.propTypes = {
   reactWrapperProps: reactWrapperProps.isRequired,
-  onSkipExtension: PropTypes.func.isRequired,
 }
 
 export default SecurityPageWrapper;
